7a9613d571629408cb700d8594a6c9f9a3bcfaab
KimRRRich <2452367222@qq.com>
2026-04-24 10:23:53 +0000

    Deploying to gh-pages from @ KimRRRich/KimRRRich.github.io@5853eea0295a74baa7b3bf4bc506f19e5ce3438d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -518,7 +518,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%78%69%61%6F%7A%68%6F%75@%68%64%75.%65%64%75.%63%6E", "_blank");
         },
       },{
         id: 'social-inspire',
